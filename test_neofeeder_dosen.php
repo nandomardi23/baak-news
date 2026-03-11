b76d814c043d94ea708eb3e39491bca0f22d948e
nandomardi23 <fernandomardi1@gmail.com>
2026-03-11 23:48:57 +0700

    chore: Remove unused development scripts and temporary files.

--- test_neofeeder_dosen.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$t = new App\Services\NeoFeederService();
-$res = $t->request('GetListPenugasanDosen', ['limit' => 2]);
-print_r($res);
